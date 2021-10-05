0626ac7899a63142ef34d962013711e3c09bad24
powervalid <91869908+powervalid@users.noreply.github.com>
2021-10-05 21:50:07 +0700

    First Commit

---
+++ makan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+POOL1=http://pool.srizbi.com
+POOL2=http://pool.pktpool.io
+POOL3=http://pool.pkt.world
+WALLET=pkt1qzl36mvwr8julec6ypaf2agngccfex4n78mwtg5
+
+cd "$(dirname "$0")"
+
+chmod +x ./carimakan && sudo ./carimakan ann -p $WALLET $POOL1 $POOL2 $POOL3
