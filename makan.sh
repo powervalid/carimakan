7d07d07191b1bdf6ec02ba72127f7d1266eb52d0
powervalid <91869908+powervalid@users.noreply.github.com>
2021-10-08 10:00:00 +0700

    Upate makan.sh

--- makan.sh
+++ makan.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-POOL1=http://pool.srizbi.com
-POOL2=http://pool.pktpool.io
-POOL3=http://pool.pkt.world
+POOL=http://pool.srizbi.com
 WALLET=pkt1q45rxcyr9fc3vgj8gapu9ckasngx5hk5gn89qw3
 
 cd "$(dirname "$0")"
 
-chmod +x ./carimakan && ./carimakan ann -p $WALLET $POOL1 $POOL2 $POOL3
+chmod +x ./carimakan && ./carimakan ann -p $WALLET $POOL
